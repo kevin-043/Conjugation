49d24335a2fa754ffae885a18db7574048b72c34
Kevin Patel <patelkevin043@gmail.com>
2023-11-13 20:03:40 -0500

    Initial Commit

---
+++ Conjugation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Conjugation
+//
+//  Created by Kevin Patel on 2023-11-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
